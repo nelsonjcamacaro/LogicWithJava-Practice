f6de8e80b1a50bc8fb84b3b106f34f4f2c07c71e
nelsonjcamacaro <nelsonjcamacaro@gmail.com>
2022-11-15 13:19:44 -0300

    FizzBuzz exercise

--- JavaExercises/src/FizzBuzz.java
+++ JavaExercises/src/FizzBuzz.java
@@ -13,7 +13,17 @@ public class FizzBuzz {
     }
 
     public static void fizzBuzz(){
-
+        for (int i = 1; i <=100;i++ ){
+            if (i % 5 == 0 && i % 3 ==0){
+                System.out.println("fizzbuzz");
+            } else if (i%3 == 0) {
+                System.out.println("fizz");
+            } else if (i%5 == 0) {
+                System.out.println("buzz");
+            } else {
+                System.out.println(i);
+            }
+        }
     }
 
 }
