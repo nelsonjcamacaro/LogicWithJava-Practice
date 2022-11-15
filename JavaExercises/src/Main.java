1d5fab08a80667fac595001b376c085907b97560
nelsonjcamacaro <nelsonjcamacaro@gmail.com>
2022-11-15 12:08:18 -0300

    first commit

---
+++ JavaExercises/src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+
+        System.out.println("Hola mundo, si, en español");
+        String name = "Nelson Camacaro";
+        System.out.println(name);
+
+        var name2 = "nelson";
+    }
+}
